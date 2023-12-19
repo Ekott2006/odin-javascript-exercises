01214a410dab3d232fcd6eb4b02a823d9d08376b
Nsikak Ekott <nsikakekott2006@gmail.com>
2023-12-19 21:22:59 +0100

    Fundamentals Part 4

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    // let result = "";
 
+    // for (let index = text.length - 1; index >= 0; index--) {
+    //   // TODO: CHECK FOR ALPHABETS ONLY
+    //   if (index.toString().match("[a-zA-Z1-9]")) {
+    //     result += text[index];
+    //   }
+    // }
+  
+    // return result == text;
 };
 
 // Do not edit below this line
