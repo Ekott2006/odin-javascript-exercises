f2bbc5297a77d0e4426553de049d24680213bd61
Nsikak Ekott <nsikakekott2006@gmail.com>
2023-12-19 22:25:54 +0100

    Exercise 12 Refactoring

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,14 +1,13 @@
 const findTheOldest = function(array) {
-    let oldest = {name:"", age: 0}
-    for (const item of array) {
-        const start = new Date(item.yearOfBirth, 0, 1)
-        const end = item.yearOfDeath ? new Date(item.yearOfDeath, 0, 1) : Date.now()
-        const age = new Date(end - start).getFullYear() -1970 
-        if (oldest.age < age) {
-            oldest = {name: item.name, age} 
-        }
+    let oldest = { name: "", age: 0 };
+  for (const item of array) {
+    const end =  Number.parseInt(item.yearOfDeath ?? new Date().getFullYear())
+    const age = end - item.yearOfBirth;
+    if (oldest.age < age) {
+      oldest = { name: item.name, age };
     }
-    return oldest
+  }
+  return oldest;
 };
 
 // Do not edit below this line
